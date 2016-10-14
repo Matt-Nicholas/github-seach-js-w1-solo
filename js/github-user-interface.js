b8ce985b47c035f944ff5dd40ed2e707cc372f61
Matt Nicholas <fostermatt82@gmial.com>
2016-10-14 11:22:48 -0700

    add function to remove contents of results div to fix multiple users displaying at once and add link to repos

--- js/github-user-interface.js
+++ js/github-user-interface.js
@@ -5,7 +5,7 @@ var getRepos = require('./../js/github-user.js').getRepos;
 $(document).ready(function(){
   $('#user-form').submit(function(event){
     event.preventDefault();
-
+    $('#display-results').empty();
     var username = $('#username').val();
     getRepos(username);
 
--- js/github-user.js
+++ js/github-user.js
@@ -2,22 +2,25 @@ var apiKey = require('./../.env').apiKey;
 
 
 exports.getRepos = function(userName){
+  //Makes api call for user info
   $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response){
     console.log(response);
-    $('#display-username').text(response.name);
+    $('#display-results').prepend('<h2 id="display-username">' + response.name + '</h2><h5>' + response.email + '</h5>');
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
+  //Makes api call for repo info
   $.get('https://api.github.com/users/' + userName + '/repos?per_page=100&access_token=' + apiKey).then(function(response){
-    $('#display-username').after('<h4><span>view public repositories</span></h4>');
+
+    $('#display-results').append('<h4><span>view public repositories</span></h4><ol id="repo-list">');
     for(var i = 0; i < response.length; i++){
       if(response[i].description === null){
         response[i].description = "this repo has no description";
       }
-      $('#repo-list').append('<li>' + response[i].name + '</li>' +
-                            '<ul><li>Description: '+ response[i].description + '</li><li>Date created: ' + response[i].created_at + '</li></ul>');
-      // console.log(response[i]);
+      $('#repo-list').append('<li><a href="' + response[i].clone_url + '">' + response[i].name + '</a></li>' + '<ul><li>Description:' + response[i].description + '</li><li>Date created: ' + response[i].created_at + '</li></ul>');
+      console.log(response[i]);
     }
+  $('#repo-list').append('</ol>');
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
